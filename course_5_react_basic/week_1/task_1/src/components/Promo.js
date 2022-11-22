63d658299a44a8e5c1589b401ace5cd6a2f2e3c2
FaRusDev <fadlirusandy@gmail.com>
2022-11-23 05:32:29 +0800

    change add course 5, week 1, task 1.4

--- course_5_react_basic/week_1/task_1/src/components/Promo.js
+++ course_5_react_basic/week_1/task_1/src/components/Promo.js
@@ -1,14 +1,22 @@
-function Promo() {
+function Promo(props) {
+
+    const styles = {
+        color: "tomato",
+        fontSize: "40px"
+    }
+    
     return (
-        <div className="promo-section">
-            <div>
-                <h1>Don't miss this deal!</h1>
+            <div className="promo-section">
+                <div>
+                    <h1 style={styles}>
+                        {props.heading}
+                    </h1>
                 </div>
                 <div>
-                <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
+                    <h2>{props.promoSubHeading}</h2>
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
 
 export default Promo;
