cef8860d31cb7ee5c4efd66efb0b30bd6e183326
FaRusDev <fadlirusandy@gmail.com>
2022-11-22 23:28:36 +0800

    change add course 5, week 1, task 1

---
+++ course_5_react_basic/week_1/task_1/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Heading(){
+  return(
+    <h1>This is an h1 heading</h1>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Heading/>
+    </div>
+  );
+}
+
+export default App;
