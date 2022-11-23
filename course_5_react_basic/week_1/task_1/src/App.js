329a78e7129a485d81e2a656057ba9fd23e173c6
FaRusDev <fadlirusandy@gmail.com>
2022-11-23 09:17:59 +0800

    change add course 5, week 1, task 1.5

--- course_5_react_basic/week_1/task_1/src/App.js
+++ course_5_react_basic/week_1/task_1/src/App.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
 import Heading from './components/Heading';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <Heading firstName="Bob" />
       <Heading firstName="Any name other than Bob" />
+
+        <h1>Task: Add three Card elements</h1>
+        <Card h2="First card's h2" h3="First card's h3" />
+        <Card h2="Second card's h2" h3="Second card's h3" />
+        <Card h2="Third card's h2" h3="Third card's h3" />
     </div>
   );
 }
