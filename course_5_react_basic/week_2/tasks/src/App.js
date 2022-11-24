c5f63095d3743ec0162231f66ff2bc3b911126d3
FaRusDev <fadlirusandy@gmail.com>
2022-11-24 09:15:16 +0800

    update task week 2

--- course_5_react_basic/week_2/tasks/src/App.js
+++ course_5_react_basic/week_2/tasks/src/App.js
@@ -1,5 +1,9 @@
 // import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import Fruits from './components/Fruits';
+import FruitsCounter from './components/FruitsCounter';
 
 function App() {
   function handleClick() {
@@ -8,12 +12,26 @@ function App() {
         let userInput = prompt('type a number');
         alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
       }
+
+    const [fruits] = React.useState([
+      {fruitName: 'apple', id: 1},
+      {fruitName: 'apple', id: 2},
+      {fruitName: 'plum', id: 3},
+    ]);
     
       return (
         <div>
           <h1>Task: Add a button and handle a click event</h1>
           <button onClick={handleClick}>Guess the number between 1 and 3</button>
-        </div>
+        
+        <div>
+          <h1>Where should the state go?</h1>
+          <Fruits fruits={fruits} />
+          <FruitsCounter fruits={fruits} />
+        </div>
+        </div> 
+
+
       );
 }
 
