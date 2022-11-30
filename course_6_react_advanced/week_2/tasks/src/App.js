2ac0478040dd9d595c2c5e669a9cdcb40e9b9e98
FaRusDev <fadlirusandy@gmail.com>
2022-11-30 11:40:30 +0800

    adding couse6, task2,

--- course_6_react_advanced/week_2/tasks/src/App.js
+++ course_6_react_advanced/week_2/tasks/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Task1 from './task1/Task1';
+import Task2 from './task2/Task2';
 
 function App() {
   return (
     <div className="App">
+      <h1>Taks 1</h1>
       <Task1/>
+      <br/>
+      <hr/>
+      <h1>Taks 2</h1>
+      <Task2/>
     </div>
   );
 }
