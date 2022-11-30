37ddff4c0b94152a472aecf65511946870f96b5b
FaRusDev <fadlirusandy@gmail.com>
2022-11-30 14:25:35 +0800

    add course 6, week 2, task3

--- course_6_react_advanced/week_2/tasks/src/App.js
+++ course_6_react_advanced/week_2/tasks/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Task1 from './task1/Task1';
 import Task2 from './task2/Task2';
+import Task3 from './task3/Task3';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <hr/>
       <h1>Taks 2</h1>
       <Task2/>
+      <hr/>
+      <h1>Taks 3</h1>
+      <Task3/>
     </div>
   );
 }
