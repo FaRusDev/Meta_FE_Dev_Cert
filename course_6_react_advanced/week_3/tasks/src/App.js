64aa7284ca3ebba09f934d848d485f4dbd7cda12
FaRusDev <fadlirusandy@gmail.com>
2022-12-01 11:30:30 +0800

    added course 6, week 3,task 2

---
+++ course_6_react_advanced/week_3/tasks/src/App.js
@@ -0,0 +1,18 @@
+import Task1 from './task1/Task1';
+import './App.css';
+import Task2 from './task2/Task2';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task 1</h1>
+      <Task1/>
+      <br/>
+      <h2>Task 2</h2>
+      <Task2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ course_6_react_advanced/week_3/tasks/src/task1/Task1.jsx
@@ -0,0 +1,20 @@
+import { RadioGroup, RadioOption } from "./Radio";
+import { useState } from "react";
+
+function Task1() {
+  const [selected, setSelected] = useState("");
+  return (
+    <div className="Task1">
+      <h2>How did you hear about Little Lemon?</h2>
+      <RadioGroup onChange={setSelected} selected={selected}>
+        <RadioOption value="social_media">Social Media</RadioOption>
+        <RadioOption value="friends">Friends</RadioOption>
+        <RadioOption value="advertising">Advertising</RadioOption>
+        <RadioOption value="other">Other</RadioOption>
+      </RadioGroup>
+      <button disabled={!selected}>Submit</button>
+    </div>
+  );
+}
+
+export default Task1;
