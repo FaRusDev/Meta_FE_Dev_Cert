0e291a3f526e3cdb3a43a55e4b286184e6309579
FaRusDev <fadlirusandy@gmail.com>
2022-12-02 16:39:49 +0800

    repair portofolio react advance

--- course_6_react_advanced/week_4/src/context/alertContext.js
+++ course_6_react_advanced/week_4/src/context/alertContext.js
@@ -1,27 +1,25 @@
-import {createContext, useContext, useState} from "react";
+import {createContext, useContext, useState} from "react"; 
 
-const AlertContext = createContext(undefined);
+const AlertContext = createContext(undefined); 
 
-export const AlertProvider = ({ children }) => {
-  const [state, setState] = useState({
-    isOpen: false,
-    // Type can be either "success" or "error"
-    type: 'success',
-    // Message to be displayed, can be any string
-    message: '',
-  });
+export const AlertProvider = ({ children }) => { 
+  const [state, setState] = useState({ 
+    isOpen: false, 
+    type: 'success', 
+    message: '', 
+  }); 
 
-  return (
-    <AlertContext.Provider
-      value={{
-        ...state,
-        onOpen: (type, message) => setState({ isOpen: true, type, message }),
-        onClose: () => setState({ isOpen: false, type: '', message: '' }),
-      }}
-    >
-      {children}
-    </AlertContext.Provider>
-  );
-};
+  return ( 
+    <AlertContext.Provider 
+      value={{ 
+        ...state, 
+        onOpen: (type, message) => setState({ isOpen: true, type, message }), 
+        onClose: () => setState({ isOpen: false, type: '', message: '' }), 
+      }} 
+    > 
+      {children} 
+    </AlertContext.Provider> 
+  ); 
+}; 
 
 export const useAlertContext = () => useContext(AlertContext);
